7ae295473f642585f90097f02ca012ff77117cfb
Aravindh NC <76737168+NC-Aravindh@users.noreply.github.com>
2022-05-30 18:19:48 +0530

    Final server js file

--- server.js
+++ server.js
@@ -8,37 +8,14 @@ const mongoose = require('mongoose');
 const ejs = require('ejs');
 const bodyParser = require("body-parser");
 
-mongoose.connect('mongodb://localhost:27017/DBZ_db', {useNewUrlParser: true,useUnifiedTopology: true});
 
-const Dataschema = new mongoose.Schema({
-  Keyword: String,
-  Tweet: String,
-  address: {
-    type: String,
-    required: [true, 'Please add an address']
-  },
-  location: {
-    type: {
-      type: String,
-      enum: ['Point']
-    },
-    coordinates: {
-      type: [Number],
-      index: '2dsphere'
-    },
-    formattedAddress: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-const Tweet = mongoose.model('Tweet', Dataschema);
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
 
 
-
-
+// NodeGeocoder authentication
 const options = {
   provider: 'here',
 
@@ -51,10 +28,24 @@ const options = {
 const geocoder = NodeGeocoder(options)
 
 
-const app = express();
+
 
 dotenv.config({ path: './config/config.env'})
 
+// Connecting to mongodb
+mongoose.connect('mongodb://localhost:27017/twit1db', {useNewUrlParser: true,useUnifiedTopology: true});
+
+const Dataschema = new mongoose.Schema({
+  keyword: String,
+  tweet: String,
+  latitude:Number,
+  longitude:Number,
+  retweet_count:Number
+})
+
+const Tweet = mongoose.model('Tweet', Dataschema);
+
+// Twitter api authentication
 const T = new twitter({
 
         consumer_key: "lCwhMzI1C8RnsIoPyBRnCyqX3",
@@ -65,110 +56,120 @@ const T = new twitter({
 
 
 
-var latitude = []
-var longitude =[]
-
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
-app.set('view engine', 'ejs');
-
-
-app.get("/geo",function(req,res){
-  res.sendFile(__dirname+"/public/home.html")
-})
-
+//Homepage route
 app.get("/",function(req,res){
   res.sendFile(__dirname+"/index.html")
 })
+
+
+
+
+app.post("/tweet",async function(req,res){
+
 var tweets = []
-// post to display the tweets
-app.post("/tweet",function(req,res){
-
-
-
-
   var key = req.body.Key;
   var no_of_tweets = req.body.limit;
 
-  T.get("search/tweets",{q:key,count:no_of_tweets} ,function(err,data,response){
+  let answer = await Tweet.find({keyword:key}, null, {limit: no_of_tweets});
 
-    for (let i = 0; i < no_of_tweets; i++) {
+  for(let x of answer){
+    tweets.push(x.tweet)
+    }
 
-            var tweet = data.statuses[i]?.text
-
-
-
-            tweets.push(tweet)
-          }
-
-
-          console.log(tweets)
-
-
-
-    })
 
  res.render("tweet",{tweet_list:tweets,Keyword:key})
 })
 
 
-//Geomap for the user locations
-app.post("/map",function(req,res){
 
-  var location_address = []
-  var tweets = []
+
+app.post("/map",async function(req,res){
+
+  var latitude = []
+  var longitude =[]
+
+
   var key = req.body.Key;
   var no_of_tweets = req.body.limit;
 
- // geocoder.geocode('Chennai,India',(err,res1)=> console.log(res1[0]?.latitude,res1[0]?.longitude))
+  let answer = await Tweet.find({keyword:key}, null, {limit: no_of_tweets});
 
-
-
-
-  T.get("search/tweets",{q:key,count:no_of_tweets} ,function(err,data,response){
-
-    for (let i = 0; i < no_of_tweets; i++) {
-            var location = data.statuses[i]?.user['location']
-            var tweet = data.statuses[i]?.text
-
-
-            location_address.push(location)
-            tweets.push(tweet)
-          }
-
-          location_address = (location_address.filter(x => x));
-
-        location_address.forEach(function(currentValue, index, arr){
-           geocoder.geocode(currentValue,(err,res1)=>{
-             // console.log(res1[0]?.latitude,res1[0]?.longitude)
-             latitude.push(res1[0]?.latitude);
-             longitude.push(res1[0]?.longitude);
-
-           })
-        })
-        // console.log(latitude,longitude)
-
-    })
+  for(let x of answer){
+    latitude.push(x.latitude)
+    longitude.push(x.longitude)
+  }
 
  res.render("map",{Lat:latitude,Long:longitude})
 })
 
 
 
+app.post("/chart",async function(req,res){
+
+  var line_points = []
+
+  var key = req.body.Key;
+  var no_of_tweets = req.body.limit;
+
+  let answer = await Tweet.find({keyword:key}, null, {limit: no_of_tweets});
+
+  for(let x of answer){
+    line_points.push(x.retweet_count)
+
+
+  }
+
+ res.render("chart",{Line_data:line_points,no_of_tweets:no_of_tweets})
+})
 
 
 
 
+app.post("/database",function(req,res){
+
+    var location_address = []
+    var tweets=[]
+    var line_data =[]
+
+    var key = req.body.Key;
+    var no_of_tweets = req.body.limit;
 
 
+// fetaching the tweets from twitter based on the keyword and the no. of tweets
+    T.get("search/tweets",{q:key,count:no_of_tweets} ,function(err,data,response){
+
+       // console.log(data)
+      for (let i = 0; i < no_of_tweets; i++) {
+              var location = data.statuses[i]?.user['location']
+              var tweet = data.statuses[i]?.text
+              var retweet_count =data.statuses[i]?.retweet_count
+
+              location_address.push(location)
+              tweets.push(tweet)
+              line_data.push(retweet_count)
+            }
+            
+            location_address = (location_address.filter(x => x));
+
+          location_address.forEach(function(currentValue, index, arr){
+
+            // geocoder converts the address to latitude and longitude
+             geocoder.geocode(currentValue,(err,res1)=>{
+
+               // adding a document to the mongodb database
+               const add_tweet = new Tweet({keyword:key,tweet:tweets[index],latitude:res1[0]?.latitude,longitude:res1[0]?.longitude,retweet_count:line_data[index]})
+               add_tweet.save();
+
+               // latitude.push(res1[0]?.latitude);
+               // longitude.push(res1[0]?.longitude);
+
+             })
+          })
 
 
-
-
-
-
-
+      })
+ res.redirect("/");//
+})
 
 app.listen(3000,function(){
 
